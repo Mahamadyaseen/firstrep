acffb398a1d2bfc08d4590ab252b0d1170a407f2
Mahamadyaseen <90181241+Mahamadyaseen@users.noreply.github.com>
2021-12-14 21:55:12 -0800

    added new files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { createContext,useState } from 'react'
+
+const context=createContext()
+
+export default function App() {
+
+  const [email, setEmail] = useState("")
+  const  [pas, setPas] = useState("")
+  return (
+    <div>
+      
+    </div>
+  )
+}
---
+++ src/component/FirstComp.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import SecComp from './SecComp'
+export default function FirstComp() {
+    return (
+        <div>
+            <SecComp/>
+        </div>
+    )
+}
